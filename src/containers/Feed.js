1f86a989c0510e413e31127a81148a9834dc66f0
roger.huang <roger.huang@udngroup.com>
2020-08-12 18:15:23 +0800

    update codebase and add pagination

--- src/containers/Feed.js
+++ src/containers/Feed.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
+import queryString from 'query-string';
+import { Link } from 'react-router-dom';
 import Card from '../components/Card/Card';
 
 const FeedWrapper = styled.div`
@@ -13,22 +15,45 @@ const Alert = styled.div`
   text-align: center;
 `;
 
+// 重新包裝React-Router的Link的樣式
+const CardLink = styled(Link)`
+  text-decoration: none;
+  color: inherit;
+`;
+
+const PaginationBar = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const PaginationLink = styled(Link)`
+  padding: 1%;
+  background: lightBlue;
+  color: white;
+  text-decoration: none;
+  border-radius: 5px;
+`;
+
 const ROOT_API = 'https://api.stackexchange.com/2.2/';
 
 class Feed extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+    const query = queryString.parse(props.location.search);
     this.state = {
       data: [],
+      page: (query.page) ? parseInt(query.page) : 1,
       loading: true,
       error: '',
     };
   }
 
   async componentDidMount() {
+    const { page } = this.state;
     try {
       const data = await fetch(
-        `${ROOT_API}questions?order=desc&sort=activity&tagged=reactjs&site=stackoverflow`,
+        `${ROOT_API}questions?order=desc&sort=activity&tagged=reactjs&site=stackoverflow${(page) ? `&page=${page}` : ''}`,
       );
       const dataJSON = await data.json();
 
@@ -47,7 +72,8 @@ class Feed extends Component {
   }
 
   render() {
-    const { data, loading, error } = this.state;
+    const { data, page, loading, error } = this.state;
+    const { match } = this.props;
 
     if (loading || error) {
       return <Alert>{loading ? 'Loading...' : error}</Alert>;
@@ -56,8 +82,14 @@ class Feed extends Component {
     return (
       <FeedWrapper>
         {data.items.map(item => (
-          <Card key={item.question_id} data={item} />
+          <CardLink key={item.question_id} to={`/questions/${item.question_id}`}>
+            <Card data={item} />
+          </CardLink>
         ))}
+        <PaginationBar>
+          {(page > 1) && (<PaginationLink to={`${match.url}?page=${page - 1}`}>Previous</PaginationLink>)}
+          {(data.has_more) && <PaginationLink to={`${match.url}?page=${page + 1}`}>Next</PaginationLink>}
+        </PaginationBar>
       </FeedWrapper>
     );
   }
