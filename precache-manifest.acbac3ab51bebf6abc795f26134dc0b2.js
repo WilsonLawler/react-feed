f9bd9daed7014853b30b19cd04344f83c35cf343
roger.huang <roger.huang@udngroup.com>
2020-08-13 17:05:42 +0800

    Updates

---
+++ precache-manifest.acbac3ab51bebf6abc795f26134dc0b2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b5e67e7cde43ed8b102ec671d5a8bd3",
+    "url": "/react-feed/index.html"
+  },
+  {
+    "revision": "efa648ffcd19a4ae8cdc",
+    "url": "/react-feed/static/js/2.ce603251.chunk.js"
+  },
+  {
+    "revision": "58d9fa75e06ee80380640756d521a664",
+    "url": "/react-feed/static/js/2.ce603251.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fb1ba1f6436ab86bfb5",
+    "url": "/react-feed/static/js/main.9b2f242f.chunk.js"
+  },
+  {
+    "revision": "2e6bd1f0d9baa0a78311",
+    "url": "/react-feed/static/js/runtime-main.b9bfc2fa.js"
+  }
+]);
